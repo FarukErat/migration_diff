06525c135cf342bb0789f7827307a051a14ab192
Faruk <farukerat1327@gmail.com>
2024-08-11 22:58:21 +0300

    merged files into one

--- generate_sql.sh
+++ generate_sql.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 
-TEMP_FILE=$(mktemp)
+MIGRATION_PLAN=$(mktemp)
 python manage.py makemigrations
-python manage.py migrate --plan > "$TEMP_FILE"
+python manage.py migrate --plan > "$MIGRATION_PLAN"
 
-OUTPUT_FOLDER="migrations_sql"
-mkdir -p "$OUTPUT_FOLDER"
+SQL_OUTPUT_FILE="migrations.sql"
+TEMP_OUTPUT=$(mktemp)
 
-index=1
-
-if [ ! -f "$TEMP_FILE" ]; then
+if [ ! -f "$MIGRATION_PLAN" ]; then
     echo "Migration plan file not found!"
     exit 1
 fi
 
-grep -E '^[a-zA-Z0-9_]+\.[0-9]+_[a-zA-Z0-9_]+$' "$TEMP_FILE" | while read -r line; do
+grep -E '^[a-zA-Z0-9_]+\.[0-9]+_[a-zA-Z0-9_]+$' "$MIGRATION_PLAN" | while read -r line; do
     app_label=$(echo "$line" | cut -d '.' -f 1)
     migration_name=$(echo "$line" | cut -d '.' -f 2-)
 
-    output_file="${OUTPUT_FOLDER}/${index}_${app_label}_${migration_name}.sql"
-
     echo "Generating SQL for migration: ${app_label}.${migration_name}"
-    python manage.py sqlmigrate "$app_label" "${migration_name}" | tee "$output_file"
-
-    index=$((index + 1))
+    python manage.py sqlmigrate "$app_label" "${migration_name}" >> "$TEMP_OUTPUT"
 done
 
-rm "$TEMP_FILE"
+mv "$TEMP_OUTPUT" "$SQL_OUTPUT_FILE"
+rm "$MIGRATION_PLAN"
