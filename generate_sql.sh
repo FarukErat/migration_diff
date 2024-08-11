e8813a1cd158148d191aa20a028446048054dff4
Faruk <farukerat1327@gmail.com>
2024-08-11 22:24:18 +0300

    removed git pull from the script

--- generate_sql.sh
+++ generate_sql.sh
@@ -1,36 +1,27 @@
 #!/bin/bash
 
-# File containing the migration plan
-PLAN_FILE="migration_plan.txt"
+OUTPUT_FILE="migration_plan.txt"
+python manage.py makemigrations
+python manage.py migrate --plan > $OUTPUT_FILE
 
-# Folder to save SQL files
 OUTPUT_FOLDER="migrations_sql"
-
-# Create the output folder if it doesn't exist
 mkdir -p "$OUTPUT_FOLDER"
 
-# Initialize an index counter
 index=1
 
-# Check if the file exists
-if [ ! -f "$PLAN_FILE" ]; then
+if [ ! -f "$OUTPUT_FILE" ]; then
     echo "Migration plan file not found!"
     exit 1
 fi
 
-# Read the plan file and extract migration names
-grep -E '^[a-zA-Z0-9_]+\.[0-9]+_[a-zA-Z0-9_]+$' "$PLAN_FILE" | while read -r line; do
-    # Extract app label and migration name
+grep -E '^[a-zA-Z0-9_]+\.[0-9]+_[a-zA-Z0-9_]+$' "$OUTPUT_FILE" | while read -r line; do
     app_label=$(echo "$line" | cut -d '.' -f 1)
     migration_name=$(echo "$line" | cut -d '.' -f 2-)
 
-    # Define the output file path with index
     output_file="${OUTPUT_FOLDER}/${index}_${app_label}_${migration_name}.sql"
 
-    # Generate SQL for the migration and save it to the output file
     echo "Generating SQL for migration: ${app_label}.${migration_name}"
     python manage.py sqlmigrate "$app_label" "${migration_name}" | tee "$output_file"
 
-    # Increment the index
     index=$((index + 1))
 done
