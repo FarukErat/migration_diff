84aaa7943b81ea00146fcb40f226eca84c308aaa
Faruk <farukerat1327@gmail.com>
2024-08-12 16:04:20 +0300

    removed redundant commands

--- generate_sql.sh
+++ generate_sql.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-SQL_OUTPUT_FILE="migrations.sql"
-MIGRATION_PLAN=$(mktemp)
-TEMP_OUTPUT=$(mktemp)
-
-python manage.py makemigrations
-python manage.py migrate --plan > "$MIGRATION_PLAN"
-
-if [ ! -f "$MIGRATION_PLAN" ]; then
-    echo "Migration plan file not found!"
-    exit 1
+SQL_OUTPUT_FILE=migrations.sql
+if [ -f $SQL_OUTPUT_FILE ]; then
+    rm $SQL_OUTPUT_FILE
 fi
 
-grep -E "^[a-zA-Z0-9_]+\\.[0-9]+_[a-zA-Z0-9_]+$" "$MIGRATION_PLAN" | while read -r line; do
+MIGRATION_PLAN=$(mktemp)
+
+python manage.py makemigrations
+python manage.py migrate --plan > $MIGRATION_PLAN
+
+grep -E "^[a-zA-Z0-9_]+\\.[0-9]+_[a-zA-Z0-9_]+$" $MIGRATION_PLAN | while read -r line; do
     app_label=$(echo "$line" | cut -d '.' -f 1)
     migration_name=$(echo "$line" | cut -d '.' -f 2-)
 
     echo "Generating SQL for migration: ${app_label}.${migration_name}"
-    python manage.py sqlmigrate "$app_label" "${migration_name}" | sed "/^BEGIN;/d; /^COMMIT;/d" >> "$TEMP_OUTPUT"
+    python manage.py sqlmigrate "$app_label" "${migration_name}" | sed "/^BEGIN;/d; /^COMMIT;/d" >> $SQL_OUTPUT_FILE
 done
 
-mv "$TEMP_OUTPUT" "$SQL_OUTPUT_FILE"
-rm "$MIGRATION_PLAN"
+rm $MIGRATION_PLAN
